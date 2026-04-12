ed4c95a23d9aaa7532f8f70b500722c5f60886c9
Ynsefkksl <143289038+Ynsefkksl@users.noreply.github.com>
2026-04-12 21:14:24 +0300

    slang updated

--- Engine/vendor/slang/cmake/slangTargets-release.cmake
+++ Engine/vendor/slang/cmake/slangTargets-release.cmake
@@ -9,11 +9,11 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET slang::slang APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(slang::slang PROPERTIES
   IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/slang-compiler.lib"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/slang-compiler.dll"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/binary/slang-compiler.dll"
   )
 
 list(APPEND _cmake_import_check_targets slang::slang )
-list(APPEND _cmake_import_check_files_for_slang::slang "${_IMPORT_PREFIX}/lib/slang-compiler.lib" "${_IMPORT_PREFIX}/bin/slang-compiler.dll" )
+list(APPEND _cmake_import_check_files_for_slang::slang "${_IMPORT_PREFIX}/lib/slang-compiler.lib" "${_IMPORT_PREFIX}/binary/slang-compiler.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
