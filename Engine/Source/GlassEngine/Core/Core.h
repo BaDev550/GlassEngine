88baa5769ea115d81ddbc892e2519383ad5af2ee
Ynsefkksl <143289038+Ynsefkksl@users.noreply.github.com>
2026-04-23 23:41:08 +0300

    some vulkan fixes, GE_ARGS fixed for clang compiler

--- Engine/Source/GlassEngine/Core/Core.h
+++ Engine/Source/GlassEngine/Core/Core.h
@@ -19,10 +19,8 @@
 #define GE_ASSERT(condition, msg) do { if (!(condition)) { GE_DEBUGBREAK(); std::cerr << msg << std::endl; } } while(0)
 	
 #define BIT(x) 1 << x
-#if defined(_MSC_VER) && !defined(__clang__)
-	#define GE_ARGS(...) ,__VA_ARGS__
 #if defined(_MSC_VER)
-	#define GE_ARGS(...) __VA_ARGS__
+	#define GE_ARGS(...) ,__VA_ARGS__
 #else
 	#define GE_ARGS(...) __VA_OPT__(, ) __VA_ARGS__
 #endif
