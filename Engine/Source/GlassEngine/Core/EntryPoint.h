a8441382727a92de86027f389a0a817dd9aea5e1
Baran Keklik <borankeklik08@gmail.com>
2026-04-24 00:03:52 +0300

    Memory leaks solved on logger

--- Engine/Source/GlassEngine/Core/EntryPoint.h
+++ Engine/Source/GlassEngine/Core/EntryPoint.h
@@ -9,5 +9,6 @@ int main() {
 		app->Run();
 		delete app;
 	}
+	_CrtDumpMemoryLeaks();
 	return 0;
 }
--- Engine/Source/GlassEngine/Utilities/Time.h
+++ Engine/Source/GlassEngine/Utilities/Time.h
@@ -3,13 +3,13 @@
 #include <chrono>
 
 namespace ge {
-	class Time {
+    class Timer {
 	public:
-		static std::string GetCurrentLocalTime() {
-			auto now = std::chrono::system_clock::now();
-			auto localTime = std::chrono::current_zone()->to_local(now);
-			auto localTimeInSeconds = std::chrono::floor<std::chrono::seconds>(localTime);
-			return std::format("{:%Y-%m-%d %H.%M.%S}", localTimeInSeconds);
-		}
-	};
+        static std::string GetCurrentLocalTime() {
+            auto now = std::chrono::system_clock::now();
+            auto localTime = std::chrono::current_zone()->to_local(now);
+            auto localTimeInSeconds = std::chrono::floor<std::chrono::seconds>(localTime);
+            return std::format("{:%Y-%m-%d %H.%M.%S}", localTimeInSeconds);
+        }
+    };
 }
