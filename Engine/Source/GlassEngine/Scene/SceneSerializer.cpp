6f47ac6577ae87bc6a1d3c2534cd3d062d46c15c
Baran Keklik <borankeklik08@gmail.com>
2026-05-02 23:56:19 +0300

    Scene serializer moved to yaml

--- Engine/Source/GlassEngine/Scene/SceneSerializer.cpp
+++ Engine/Source/GlassEngine/Scene/SceneSerializer.cpp
@@ -2,99 +2,214 @@
 #include "SceneSerializer.h"
 #include <set>
 
+#include <yaml-cpp/yaml.h>
+#include <yaml-cpp/binary.h>
+
+namespace YAML {
+	template<>
+	struct convert<glm::vec2> {
+		static Node encode(const glm::vec2& rhs) {
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec2& rhs) {
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec3> {
+		static Node encode(const glm::vec3& rhs) {
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec3& rhs) {
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec4> {
+		static Node encode(const glm::vec4& rhs) {
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec4& rhs) {
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<ge::UUID> {
+		static Node encode(const ge::UUID& uuid) {
+			Node node;
+			node.push_back((uint64_t)uuid);
+			return node;
+		}
+		static bool decode(const Node& node, ge::UUID& uuid) {
+			uuid = node[0].as<uint64_t>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<GEString> {
+		static Node encode(const GEString& str) {
+			Node node;
+			node.push_back(str);
+			return node;
+		}
+		static bool decode(const Node& node, GEString& str) {
+			str = node[0].as<std::string>();
+			return true;
+		}
+	};
+}
 namespace ge {
-	static void SerializeEntity(file::Writer& out, Entity& entity) {
-		EntityID entityId = entity.GetEntityID();
-		out.WriteData(reinterpret_cast<const char*>(&entityId), sizeof(EntityID));
-
-		uint32_t entityNameLenght = entity.GetName().size();
-		out.WriteData(reinterpret_cast<const char*>(&entityNameLenght), sizeof(uint32_t));
-		out.WriteData(entity.GetName().c_str(), entityNameLenght);
-
-		auto& tc = entity.GetComponent<TransformComponent>();
-		out.WriteData(reinterpret_cast<const char*>(&tc.position), sizeof(glm::vec3));
-		out.WriteData(reinterpret_cast<const char*>(&tc.rotation), sizeof(glm::vec3));
-		out.WriteData(reinterpret_cast<const char*>(&tc.scale), sizeof(glm::vec3));
-
-		bool hasStaticMesh = entity.HasComponent<StaticMeshComponent>();
-		out.WriteData(reinterpret_cast<const char*>(&hasStaticMesh), sizeof(bool));
-
-		if (hasStaticMesh) {
-			auto& smc = entity.GetComponent<StaticMeshComponent>();
-			out.WriteData(reinterpret_cast<const char*>(&smc.meshHandle), sizeof(AssetHandle));
-			out.WriteData(reinterpret_cast<const char*>(&smc.lodLevel), sizeof(uint32_t));
-			out.WriteData(reinterpret_cast<const char*>(&smc.isVisible), sizeof(bool));
-		}
+	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& vec) {
+		out << YAML::Flow;
+		out << YAML::BeginSeq << vec.x << vec.y << YAML::EndSeq;
+		return out;
 	}
-	static std::pair<EntityID, GEString> GetEntityNameAndID(file::Reader& reader) {
-		EntityID id;
-		uint32_t entityNameLenght = 0;
-		reader.ReadData(reinterpret_cast<char*>(&id), sizeof(EntityID));
-		reader.ReadData(reinterpret_cast<char*>(&entityNameLenght), sizeof(uint32_t));
-		GEString entityName;
-		entityName.resize(entityNameLenght);
-		reader.ReadData(entityName.data(), entityNameLenght);
 
-		return { id, entityName };
+	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& vec) {
+		out << YAML::Flow;
+		out << YAML::BeginSeq << vec.x << vec.y << vec.z << YAML::EndSeq;
+		return out;
 	}
-	static void DeserializeEntity(file::Reader& reader, Entity& entity) {
-		auto& tc = entity.GetComponent<TransformComponent>();
-		reader.ReadData(reinterpret_cast<char*>(&tc.position), sizeof(glm::vec3));
-		reader.ReadData(reinterpret_cast<char*>(&tc.rotation), sizeof(glm::vec3));
-		reader.ReadData(reinterpret_cast<char*>(&tc.scale), sizeof(glm::vec3));
 
-		bool hasStaticMesh = false;
-		reader.ReadData(reinterpret_cast<char*>(&hasStaticMesh), sizeof(bool));
+	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& vec) {
+		out << YAML::Flow;
+		out << YAML::BeginSeq << vec.x << vec.y << vec.z << vec.w << YAML::EndSeq;
+		return out;
+	}
 
-		if (hasStaticMesh) {
-			auto& smc = entity.AddComponent<StaticMeshComponent>();
-			reader.ReadData(reinterpret_cast<char*>(&smc.meshHandle), sizeof(AssetHandle));
-			reader.ReadData(reinterpret_cast<char*>(&smc.lodLevel), sizeof(uint32_t));
-			reader.ReadData(reinterpret_cast<char*>(&smc.isVisible), sizeof(bool));
-		}
+	YAML::Emitter& operator<<(YAML::Emitter& out, const GEString& string) {
+		out << (std::string)string;
+		return out;
+	}
+
+#define SERIALIZE_COMPONENT_START(component) if (entity.HasComponent<component>()) { \
+	out << YAML::Key << #component; \
+	out << YAML::BeginMap; \
+	component& comp_##component = entity.GetComponent<component>();
+#define SERIALIZE_COMPONENT_END out << YAML::EndMap; }
+
+#define DESERIALIZE_COMPONENT_START(component) auto node_##component = node[#component]; if(node_##component) { auto& comp_##component = entity.AddOrReplaceComponent<component>();
+#define DESERIALIZE_COMPONENT_END }
+
+	static void SerializeEntity(YAML::Emitter& out, Entity& entity) {
+		out << YAML::BeginMap;
+		out << YAML::Key << "Entity" << YAML::Value << entity.GetEntityID();
+
+		SERIALIZE_COMPONENT_START(IdentityComponent)
+			out << YAML::Key << "Name" << YAML::Value << comp_IdentityComponent.name;
+		SERIALIZE_COMPONENT_END
+
+		SERIALIZE_COMPONENT_START(TransformComponent)
+			out << YAML::Key << "Position" << YAML::Value << comp_TransformComponent.position;
+			out << YAML::Key << "Rotation" << YAML::Value << comp_TransformComponent.rotation;
+			out << YAML::Key << "Scale" << YAML::Value << comp_TransformComponent.scale;
+		SERIALIZE_COMPONENT_END
+
+		SERIALIZE_COMPONENT_START(StaticMeshComponent)
+			out << YAML::Key << "MeshHandle" << YAML::Value << comp_StaticMeshComponent.meshHandle;
+			out << YAML::Key << "IsVisible" << YAML::Value << comp_StaticMeshComponent.isVisible;
+			out << YAML::Key << "LODLevel" << YAML::Value << comp_StaticMeshComponent.lodLevel;
+		SERIALIZE_COMPONENT_END
+
+		out << YAML::EndMap;
+	}
+	
+	static void DeserializeEntity(YAML::Node node, Entity& entity) {
+		DESERIALIZE_COMPONENT_START(TransformComponent)
+			comp_TransformComponent.position = node_TransformComponent["Position"].as<glm::vec3>();
+			comp_TransformComponent.rotation = node_TransformComponent["Rotation"].as<glm::vec3>();
+			comp_TransformComponent.scale = node_TransformComponent["Scale"].as<glm::vec3>();
+		DESERIALIZE_COMPONENT_END
+
+		DESERIALIZE_COMPONENT_START(StaticMeshComponent)
+			comp_StaticMeshComponent.meshHandle = node_StaticMeshComponent["MeshHandle"].as<uint64_t>();
+			comp_StaticMeshComponent.isVisible = node_StaticMeshComponent["IsVisible"].as<bool>();
+			comp_StaticMeshComponent.lodLevel = node_StaticMeshComponent["LODLevel"].as<uint32_t>();
+		DESERIALIZE_COMPONENT_END
 	}
 
 	SceneSerializer::SceneSerializer(const mem::Ref<Scene>& scene) : _scene(scene) { }
 
 	bool SceneSerializer::Serialize(const GEString& path)
 	{
-		file::Writer out(path.ToPath());
-		if (!out.IsStreamGood())
-			return false;
-		uint32_t sceneNameLength = _scene->GetName().size();
-		uint32_t entityCount = _scene->GetEntities().size();
-		out.WriteData(reinterpret_cast<const char*>(&sceneNameLength), sizeof(uint32_t));
-		out.WriteData(_scene->GetName().c_str(), sceneNameLength);
-		out.WriteData(reinterpret_cast<const char*>(&entityCount), sizeof(uint32_t));
-
+		YAML::Emitter out;
+		out << YAML::BeginMap;
+		out << YAML::Key << "Scene" << YAML::Value << _scene->GetName();
+		out << YAML::Key << "Entities" << YAML::Value << YAML::BeginSeq;
 		for (auto& [id, entity] : _scene->GetEntities()) {
 			if (!entity)
 				continue;
 
 			SerializeEntity(out, *entity);
 		}
+		YAML::EndSeq;
+		YAML::EndMap;
+
+		std::ofstream file(path);
+		file << out.c_str();
 		return true;
 	}
 
 	bool SceneSerializer::Deserialize(const GEString& path)
 	{
 		_scene->Clear();
-		file::Reader in(path.ToPath());
-		if (!in.IsStreamGood())
+		std::ifstream stream(path);
+		std::stringstream strStream;
+		strStream << stream.rdbuf();
+
+		YAML::Node data = YAML::Load(strStream.str());
+		if (!data["Scene"])
 			return false;
 
-		uint32_t sceneNameSize = 0;
-		uint32_t entityCount = 0;
-		in.ReadData(reinterpret_cast<char*>(&sceneNameSize), sizeof(uint32_t));
-		GEString sceneName;
-		sceneName.resize(sceneNameSize);
-		in.ReadData(sceneName.data(), sceneNameSize);
-		in.ReadData(reinterpret_cast<char*>(&entityCount), sizeof(uint32_t));
+		GEString sceneName = data["Scene"].as<std::string>();
+		auto entities = data["Entities"];
+		if (entities) {
+			for (auto entity : entities) {
+				UUID uuid = entity["Entity"].as<uint64_t>();
+				GEString name;
+				auto identityComponent = entity["IdentityComponent"];
+				if (identityComponent) {
+					name = identityComponent["Name"].as<std::string>();
+				}
+				Entity* deserializedEntity = _scene->CreateEntityWithID(uuid, name);
 
-		for (uint32_t i = 0; i < entityCount; i++) {
-			std::pair<EntityID, GEString> idName = GetEntityNameAndID(in);
-			auto entity = _scene->CreateEntityWithID(idName.first, idName.second);
-			DeserializeEntity(in, *entity);
+				if (deserializedEntity)
+					DeserializeEntity(entity, *deserializedEntity);
+			}
 		}
 		return true;
 	}
